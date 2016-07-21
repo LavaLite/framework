712db4e28a58c5440ff5b5005b36d0d178b2f8e0
George John <george@renfos.com>
2016-07-21 10:25:02 +0530

    update user home

--- src/Litepie/User/resources/views/public/default/home.blade.php
+++ src/Litepie/User/resources/views/public/default/home.blade.php
@@ -1,123 +1,148 @@
-
 @include('public::notifications')
 <div class="row">
     <div class="col-sm-6 col-lg-3">
         <div class="card-box widget-icon bg-success">
             <div>
-                <i class="icon-speech text-white "></i>
+                <i class="icon-speech text-white ">
+                </i>
                 <div class="wid-icon-info">
-                    <p class="text-white m-b-5 font-13 text-uppercase">My Blogs</p>
-                    <h4 class="m-t-0 m-b-5 counter text-white">1452</h4>
-                    <small class="text-white "><b>39% Up</b></small>
+                    <p class="text-white m-b-5 font-13 text-uppercase">
+                        My Blogs
+                    </p>
+                    <h4 class="m-t-0 m-b-5 counter text-white">
+                        {{Blog::count('blog')}}
+                    </h4>
+                    <small class="text-white "><b></b></small>
                 </div>
             </div>
         </div>
     </div>
-
     <div class="col-sm-6 col-lg-3">
         <div class="card-box widget-icon bg-danger">
             <div>
-                <i class="icon-user text-white"></i>
+                <i class="icon-user text-white">
+                </i>
                 <div class="wid-icon-info">
-                    <p class="text-white m-b-5 font-13 text-uppercase">Testimoials</p>
-                    <h4 class="m-t-0 m-b-5 counter text-white">1452</h4>
-                    <small class="text-white"><b>56% Down</b></small>
+                    <p class="text-white m-b-5 font-13 text-uppercase">
+                        Discussions
+                    </p>
+                    <h4 class="m-t-0 m-b-5 counter text-white">
+                        {{Forum::count()}}
+                    </h4>
+                    <small class="text-white "><b></b></small>
                 </div>
             </div>
         </div>
     </div>
-
     <div class="col-sm-6 col-lg-3">
         <div class="card-box widget-icon bg-pink">
             <div>
-                <i class="icon-book-open text-white"></i>
+                <i class="icon-book-open text-white">
+                </i>
                 <div class="wid-icon-info">
-                    <p class="text-white m-b-5 font-13 text-uppercase">News</p>
-                    <h4 class="m-t-0 m-b-5 counter text-white">1452</h4>
-                    <small class="text-white"><b>0%</b></small>
+                    <p class="text-white m-b-5 font-13 text-uppercase">
+                        News
+                    </p>
+                    <h4 class="m-t-0 m-b-5 counter text-white">
+                        {{News::count()}}
+                    </h4>
+                    <small class="text-white "><b></b></small>
                 </div>
             </div>
         </div>
     </div>
-
     <div class="col-sm-6 col-lg-3">
         <div class="card-box widget-icon bg-purple">
             <div>
-                <i class="icon-picture text-white"></i>
+                <i class="icon-picture text-white">
+                </i>
                 <div class="wid-icon-info">
-                    <p class="text-white m-b-5 font-13 text-uppercase">My Gallery</p>
-                    <h4 class="m-t-0 m-b-5 counter text-white">562</h4>
-                    <small class="text-white"><b>39% Up</b></small>
+                    <p class="text-white m-b-5 font-13 text-uppercase">
+                        My Gallery
+                    </p>
+                    <h4 class="m-t-0 m-b-5 counter text-white">
+                        {{Gallery::count()}}
+                    </h4>
+                    <small class="text-white "><b></b></small>
                 </div>
             </div>
         </div>
     </div>
-
 </div>
-<div class="dashboard-content  m-t-5">
-    <div class="panel panel-color panel-inverse">
-        <div class="panel-heading">
-            <h3 class="panel-title">My <span>Blogs</span></h3>
-            <p class="panel-sub-title m-t-5 text-muted">Sub title goes here with small font</p>
+<div class="row">
+    <div class="col-sm-7 col-lg-7">
+        <div class="dashboard-content">
+            <div class="panel panel-color panel-purple">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-6">
+                            <h4 class="panel-title" style="font-size: 16px;">
+                                Calendar
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel-body">
+                    {!!Calendar::gadget('user.calendar.gadget')!!}
+                    <div class="row m-t-20">
+                        <div class="col-md-12">
+                            <a class="btn btn-sm btn-danger waves-effect waves-light text-uppercase" href="{!! trans_url('user/calendar/calendar') !!}">
+                                View All
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="panel-body">
-            <div class="popular-post-block">
-                <div class="row">
-                    <div class="dashboard-blog-pic">
-                        <img src="img/news1.jpg" class="img-responsive" alt="">
-                    </div>
-                    <div class="dashboard-blog-desc popular-post-inner">
-                        <div class="popular-post-desc">
-                            <a href=""><h4>Aenean a mi et risus facilisis maximus</h4></a>
-                            <p>Mon , 03 / 23 / 2016</p>
+    </div>
+    <div class="col-md-5">
+        <div class="dashboard-content">
+            <div class="panel panel-color panel-success">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-6">
+                            <h4 class="panel-title" style="font-size: 16px;">
+                                     Discussions
+                            </h4>
                         </div>
                     </div>
-                    <div class="dashboard-blog-actions  text-right">
-                        <a href="news-detail.html" class="btn btn-icon waves-effect btn-success m-b-5"><i class="fa fa-eye"></i></a>
-                        <a href="my-news-edit.html" class="btn btn-icon waves-effect btn-primary m-b-5"><i class="fa fa-pencil"></i></a>
-                        <a href="#" class="btn btn-icon waves-effect btn-danger"><i class="fa fa-trash"></i></a>
+                </div>
+                <div class="panel-body">
+                    {!!Forum::gadget('user.forum.gadget')!!}
+                    <div class="row m-t-20">
+                        <div class="col-md-12">
+                            <a class="btn btn-sm btn-danger waves-effect waves-light text-uppercase" href="{!! trans_url('user/forum/forum') !!}">
+                                View All
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <div class="popular-post-block">
-                <div class="row">
-                    <div class="dashboard-blog-pic">
-                        <img src="img/news2.jpg" class="img-responsive" alt="">
-                    </div>
-                    <div class="dashboard-blog-desc popular-post-inner">
-                        <div class="popular-post-desc">
-                            <a href=""><h4>Aenean a mi et risus facilisis maximus</h4></a>
-                            <p>Mon , 03 / 23 / 2016</p>
+        </div>
+    </div>
+    <div class="col-md-5">
+        <div class="dashboard-content">
+            <div class="panel panel-color panel-pink">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-6">
+                            <h4 class="panel-title" style="font-size: 16px;">
+                                News
+                            </h4>
                         </div>
                     </div>
-                    <div class="dashboard-blog-actions  text-right">
-                        <a href="news-detail.html" class="btn btn-icon waves-effect btn-success m-b-5"><i class="fa fa-eye"></i></a>
-                        <a href="my-news-edit.html" class="btn btn-icon waves-effect btn-primary m-b-5"><i class="fa fa-pencil"></i></a>
-                        <a href="#" class="btn btn-icon waves-effect btn-danger"><i class="fa fa-trash"></i></a>
+                </div>
+                <div class="panel-body">
+                    {!!News::gadget('user.news.gadget')!!}
+                    <div class="row m-t-20">
+                        <div class="col-md-12">
+                            <a class="btn btn-sm btn-danger waves-effect waves-light text-uppercase" href="{!! trans_url('user/news/news') !!}">
+                                View All
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <div class="popular-post-block">
-                <div class="row">
-                    <div class="dashboard-blog-pic">
-                        <img src="img/news3.jpg" class="img-responsive" alt="">
-                    </div>
-                    <div class="dashboard-blog-desc popular-post-inner">
-                        <div class="popular-post-desc">
-                            <a href=""><h4>Aenean a mi et risus facilisis maximus</h4></a>
-                            <p>Mon , 03 / 23 / 2016</p>
-                        </div>
-                    </div>
-                    <div class="dashboard-blog-actions  text-right">
-                        <a href="news-detail.html" class="btn btn-icon waves-effect btn-success m-b-5"><i class="fa fa-eye"></i></a>
-                        <a href="my-news-edit.html" class="btn btn-icon waves-effect btn-primary m-b-5"><i class="fa fa-pencil"></i></a>
-                        <a href="#" class="btn btn-icon waves-effect btn-danger"><i class="fa fa-trash"></i></a>
-                    </div>
-                </div>
-            </div>
-
         </div>
     </div>
 </div>
