5dc2ce2a827ece2c2da2da9b3889bf2596116b6e
George John <george@renfos.com>
2016-07-29 15:32:33 +0530

    Update for single file display

--- src/Litepie/Filer/views/single/show.blade.php
+++ src/Litepie/Filer/views/single/show.blade.php
@@ -4,12 +4,12 @@
         $ext  = strtolower($info['extension']);
     ?>
     @if (in_array($ext, ['jpg','jpeg', 'png', 'gif']) )
-        <a href='{!! url("/file/" . folder_encode($files["folder"]))!!}/{!!$files["file"]!!}' target="_blank">
+        <a href='{!! url("/file/{$config}/".folder_encode($files["folder"]))!!}/{!!$files["file"]!!}' target="_blank">
         <img src='{!! url("/image/{$size}/{$config}/".folder_encode($files["folder"]))!!}/{!! $files["file"] !!}' class="img-thumbnail image-responsive">
         </a>
     @else
         <div id="file">
-            <a href='{!! url("/file/" . folder_encode($files["folder"]))!!}/{!!$files["file"]!!}' target="_blank">{!!$files["file"]!!}</a>
+            <a href='{!! url("/file/{$config}/".folder_encode($files["folder"]))!!}/{!!$files["file"]!!}' target="_blank">{!!$files["file"]!!}</a>
         </div>
     @endif
 @else
