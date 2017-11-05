18283d62441dd029c0f6a99293bf55bb2f6aaa76
George John <george@renfos.com>
2017-11-05 22:47:26 +0530

    User migration bug fix

--- src/Litepie/User/database/migrations/2015_01_05_100010_create_password_resets_table.php
+++ src/Litepie/User/database/migrations/2015_01_05_100010_create_password_resets_table.php
@@ -15,7 +15,7 @@ class CreatePasswordResetsTable extends Migration
         Schema::create('password_resets', function (Blueprint $table) {
             $table->string('email')->index();
             $table->string('token')->index();
-            $table->nullableTimestamps('created_at');
+            $table->nullableTimestamps();
         });
     }
 
