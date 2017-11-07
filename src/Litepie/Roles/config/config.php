953b5acfed4b187926f78bd892312f46cebbf16f
George John <george@renfos.com>
2017-11-07 09:07:47 +0530

    Updated roles config

--- src/Litepie/Roles/config/config.php
+++ src/Litepie/Roles/config/config.php
@@ -51,56 +51,90 @@ return [
         ],
     ],
 
-
     'permission' => [
-        'model'         => Litepie\User\Models\Permission::class,
-        'table'         => 'permissions',
-        'presenter'     => \Litepie\User\Repositories\Presenter\PermissionItemPresenter::class,
-        'hidden'        => [],
-        'visible'       => [],
-        'guarded'       => ['*'],
-        'slugs'         => ['slug' => 'name'],
-        'dates'         => ['deleted_at'],
-        'appends'       => [],
-        'fillable'      => ['slug', 'name'],
-        'translate'     => [],
-        'upload-folder' => 'user/permission',
-        'uploads'       => [],
-        'casts'         => [],
-        'revision'      => [],
-        'perPage'       => '20',
-        'search'        => [
-            'name' => 'like',
-            'status',
+        'model' => [
+            'model'         => \Litepie\Roles\Models\Permission::class,
+            'table'         => 'permissions',
+            'presenter'     => \Litepie\Roles\Repositories\Presenter\PermissionItemPresenter::class,
+            'hidden'        => [],
+            'visible'       => [],
+            'guarded'       => ['*'],
+            'slugs'         => ['slug' => 'name'],
+            'dates'         => ['deleted_at'],
+            'appends'       => [],
+            'fillable'      => ['slug', 'name'],
+            'translate'     => [],
+            'upload-folder' => 'user/permission',
+            'uploads'       => [],
+            'casts'         => [],
+            'revision'      => [],
+            'perPage'       => '20',
+            'search'        => [
+                'name' => 'like',
+                'status',
+            ],
         ],
     ],
 
     'role'       => [
-        'model'         => \Litepie\User\Models\Role::class,
-        'table'         => 'roles',
-        'presenter'     => \Litepie\User\Repositories\Presenter\RoleItemPresenter::class,
-        'hidden'        => [],
-        'visible'       => [],
-        'guarded'       => ['*'],
-        'slugs'         => [],
-        'dates'         => ['deleted_at'],
-        'appends'       => [],
-        'fillable'      => ['key', 'name', 'permissions'],
-        'translate'     => [],
+        'model' => [
+            'model'         => \Litepie\Roles\Models\Role::class,
+            'table'         => 'roles',
+            'presenter'     => \Litepie\Roles\Repositories\Presenter\RoleItemPresenter::class,
+            'hidden'        => [],
+            'visible'       => [],
+            'guarded'       => ['*'],
+            'slugs'         => [],
+            'dates'         => ['deleted_at'],
+            'appends'       => [],
+            'fillable'      => ['key', 'name', 'permissions'],
+            'translate'     => [],
 
-        'upload-folder' => 'user/role',
-        'uploads'       => [],
-        'casts'         => [
-            'permissions' => 'array',
-        ],
-        'revision'      => [],
-        'perPage'       => '20',
-        'search'        => [
-            'name'       => 'like',
-            'key'        => 'like',
-            'created_at' => 'like',
-            'updated_at' => 'like',
+            'upload-folder' => 'user/role',
+            'uploads'       => [],
+            'casts'         => [
+                'permissions' => 'array',
+            ],
+            'revision'      => [],
+            'perPage'       => '20',
+            'search'        => [
+                'name'       => 'like',
+                'key'        => 'like',
+                'created_at' => 'like',
+                'updated_at' => 'like',
+            ],
         ],
 
     ],
+    /*
+    |--------------------------------------------------------------------------
+    | Slug Separator
+    |--------------------------------------------------------------------------
+    |
+    | Here you can change the slug separator. This is very important in matter
+    | of magic method __call() and also a `Slugable` trait. The default value
+    | is a dot.
+    |
+     */
+    'separator'  => '.',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Roles, Permissions and Allowed "Pretend"
+    |--------------------------------------------------------------------------
+    |
+    | You can pretend or simulate package behavior no matter what is in your
+    | database. It is really useful when you are testing you application.
+    | Set up what will methods is(), can() and allowed() return.
+    |
+     */
+    'pretend'    => [
+        'enabled' => true,
+        'options' => [
+            'is'      => true,
+            'can'     => true,
+            'allowed' => true,
+        ],
+    ],
+
 ];
