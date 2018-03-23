d9d89bb02c061c1d953b2270c4b9e400f41171b0
George John <george@renfos.com>
2018-03-23 17:27:30 +0530

    Updates core package builder

--- build/pie-split.sh
+++ build/pie-split.sh
@@ -4,7 +4,6 @@
 
 ./git-subsplit.sh init git@github.com:lavalite/framework.git
 ./git-subsplit.sh publish --no-tags src/Litepie/Activities:git@github.com:Litepie/Activities.git
-./git-subsplit.sh publish --no-tags src/Litepie/Contracts:git@github.com:Litepie/contracts.git
 ./git-subsplit.sh publish --no-tags src/Litepie/Database:git@github.com:Litepie/database.git
 ./git-subsplit.sh publish --no-tags src/Litepie/Filer:git@github.com:Litepie/filer.git
 ./git-subsplit.sh publish --no-tags src/Litepie/Form:git@github.com:Litepie/form.git
@@ -12,8 +11,10 @@
 ./git-subsplit.sh publish --no-tags src/Litepie/Menu:git@github.com:Litepie/menu.git
 ./git-subsplit.sh publish --no-tags src/Litepie/Node:git@github.com:Litepie/node.git
 ./git-subsplit.sh publish --no-tags src/Litepie/Repository:git@github.com:Litepie/repository.git
-./git-subsplit.sh publish --no-tags src/Litepie/Revision:git@github.com:Litepie/revision.git
+./git-subsplit.sh publish --no-tags src/Litepie/Roles:git@github.com:Litepie/roles.git
 ./git-subsplit.sh publish --no-tags src/Litepie/Settings:git@github.com:Litepie/settings.git
+./git-subsplit.sh publish --no-tags src/Litepie/Team:git@github.com:Litepie/team.git
+./git-subsplit.sh publish --no-tags src/Litepie/Theme:git@github.com:Litepie/theme.git
 ./git-subsplit.sh publish --no-tags src/Litepie/Trans:git@github.com:Litepie/trans.git
 ./git-subsplit.sh publish --no-tags src/Litepie/User:git@github.com:Litepie/user.git
 rm -rf .subsplit/
