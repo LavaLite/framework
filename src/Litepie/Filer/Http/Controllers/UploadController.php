3d729beeb3821dc2bbfbe3d4d1b5c16c6926e9d4
George John <george@renfos.com>
2018-04-10 17:36:58 +0530

    Added new url on return array for image and file path.

--- src/Litepie/Filer/Http/Controllers/UploadController.php
+++ src/Litepie/Filer/Http/Controllers/UploadController.php
@@ -28,8 +28,17 @@ class UploadController extends Controller
         if (Request::hasFile($file)) {
             $ufolder         = $this->uploadFolder($config, $folder, $field);
             $array           = Filer::upload(Request::file($file), $ufolder);
-            $array['folder'] = ($folder) . '/' . $field;
+            $array['folder'] = $folder . '/' . $field;
             $array['path']   = $ufolder . '/' . $array['file'];
+
+            $ext = pathinfo($array['file'], PATHINFO_EXTENSION);
+
+            if (in_array($ext, config('filer.image_extensions'))) {
+                $array['url'] = url('image/original' . $ufolder . '/' . $array['file']);
+            } else {
+                $array['url'] = url('file/display' . $ufolder . '/' . $array['file']);
+            }
+
             return response()->json($array)
                 ->setStatusCode(203, 'UPLOAD_SUCCESS');
         }
