037f24f682ff4c75cc80c686acf88a2d8b456c4f
George John <george@renfos.com>
2018-10-31 17:47:52 +0530

    Added re-captcha validator

---
+++ src/Litepie/Validators/ReCaptcha.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Litepie\Validators;
+
+use GuzzleHttp\Client;
+
+class ReCaptcha
+{
+    public function validate(
+        $attribute,
+        $value,
+        $parameters,
+        $validator
+    ){
+
+        $client = new Client();
+
+        $response = $client->post('https://www.google.com/recaptcha/api/siteverify',
+            ['form_params'=>
+                 [
+                     'secret'=>config('services.recaptcha.secret'),
+                     'response'=>$value,
+                     'remoteip'=>$_SERVER['REMOTE_ADDR'],
+                 ]
+            ]
+        );
+
+        $body = json_decode((string)$response->getBody());
+        return $body->success;
+    }
+
+}
---
+++ src/Litepie/Validators/ValidatorServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Litepie\Validators;
+
+use Illuminate\Support\ServiceProvider;
+use Validator;
+class ValidatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Load view
+        Validator::extend(
+               'recaptcha',
+               'Litepie\\Validators\\ReCaptcha@validate'
+        );
+    }
+
+}
