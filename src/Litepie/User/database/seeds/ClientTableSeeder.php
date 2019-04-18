9b0179f7113ed2e62bc3a725d566a9a26ac05e24
George John <george@renfos.com>
2019-04-18 15:44:34 +0530

    Updated client and user table seeder.

--- src/Litepie/User/database/seeds/ClientTableSeeder.php
+++ src/Litepie/User/database/seeds/ClientTableSeeder.php
@@ -11,17 +11,18 @@ class ClientTableSeeder extends Seeder
     {
         DB::table('clients')->insert([
             [
-                'id'          => 1,
-                'email'       => 'client@lavalite.org',
-                'password'    => bcrypt('client@lavalite'),
-                'status'      => 'Active',
-                'name'        => 'Client',
-                'sex'         => 'Male',
-                'dob'         => '2014-05-15',
-                'api_token'   => str_random(60),
-                'designation' => 'Super User',
-                'web'         => 'http://litepie.org',
-                'created_at'  => '2015-09-15',
+                'id'                => 1,
+                'email'             => 'client@lavalite.org',
+                'password'          => bcrypt('client@lavalite'),
+                'status'            => 'Active',
+                'name'              => 'Client',
+                'sex'               => 'Male',
+                'dob'               => '2014-05-15',
+                'api_token'         => str_random(60),
+                'designation'       => 'Super User',
+                'email_verified_at' => '2019-04-15 00:00:00',
+                'web'               => 'http://litepie.org',
+                'created_at'        => '2015-09-15',
             ],
         ]);
     }
