5d4076882c1d400703fb0574a02de1612925f291
HaTV <hatv1592@gmail.com>
2020-01-31 14:51:18 +0700

    Update SubMenuResourceController.php
    'parent' dont use will make error in php 7.3 and later.

--- src/Litepie/Menu/Http/Controllers/SubMenuResourceController.php
+++ src/Litepie/Menu/Http/Controllers/SubMenuResourceController.php
@@ -29,7 +29,7 @@ class SubMenuResourceController extends ResourceController
         $menu = $this->repository->find($id);
         Form::populate($menu);
 
-        return view('menu::sub.show', compact('parent', 'menu'));
+        return view('menu::sub.show', compact('menu'));
     }
 
     /**
