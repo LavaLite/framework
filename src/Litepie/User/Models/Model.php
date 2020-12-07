60d99d8fa3596fa8426a456eb449843d387a3582
George John <george@renfos.com>
2020-12-07 22:03:43 +0400

    Fixed hash error.

--- src/Litepie/User/Models/Model.php
+++ src/Litepie/User/Models/Model.php
@@ -2,6 +2,7 @@
 
 namespace Litepie\User\Models;
 
+use Hash;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
