8c1c8a2f59d85607853e2b1da3e5adae4d852445
George John <george@renfos.com>
2021-09-23 13:01:50 +0400

    Style fix

--- src/Litepie/Filer/config.php
+++ src/Litepie/Filer/config.php
@@ -12,8 +12,8 @@ return [
     | Recommendation: storage/uploads/
     |
      */
-    'folder'            => env('UPLOAD_FOLDER', 'storage/uploads'),
-    'storage'           => env('UPLOAD_TARGET', 'local'),
+    'folder' => env('UPLOAD_FOLDER', 'storage/uploads'),
+    'storage' => env('UPLOAD_TARGET', 'local'),
     'folder_permission' => 0777, // Default 0777 - Other likely values 0775, 0755
 
     /*
