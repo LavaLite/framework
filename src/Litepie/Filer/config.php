b9a467956200989367666e4c8ee8865130e1e54e
George John <georgemjohn@users.noreply.github.com>
2021-09-23 09:10:05 +0000

    Apply fixes from StyleCI

--- src/Litepie/Filer/config.php
+++ src/Litepie/Filer/config.php
@@ -12,8 +12,8 @@ return [
     | Recommendation: storage/uploads/
     |
      */
-    'folder' => env('UPLOAD_FOLDER', 'storage/uploads'),
-    'storage' => env('UPLOAD_TARGET', 'local'),
+    'folder'            => env('UPLOAD_FOLDER', 'storage/uploads'),
+    'storage'           => env('UPLOAD_TARGET', 'local'),
     'folder_permission' => 0777, // Default 0777 - Other likely values 0775, 0755
 
     /*
@@ -26,7 +26,7 @@ return [
      */
 
     'allowed_types_check' => false,
-    'allowed_types' => [
+    'allowed_types'       => [
         'application/msword',
         'application/pdf',
         'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
