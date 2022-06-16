7656014623d54d17ea199c1aec959cb357c4a07c
George John <george@renfos.com>
2022-06-16 12:25:19 +0400

    Update filer

--- src/Litepie/Filer/config.php
+++ src/Litepie/Filer/config.php
@@ -12,9 +12,10 @@ return [
     | Recommendation: storage/uploads/
     |
      */
-    'folder'            => env('UPLOAD_FOLDER', 'storage/uploads'),
-    'storage'           => env('UPLOAD_TARGET', 'local'),
+    'folder' => env('UPLOAD_FOLDER', 'storage/uploads'),
+    'storage' => env('UPLOAD_TARGET', 'local'),
     'folder_permission' => 0777, // Default 0777 - Other likely values 0775, 0755
+    'default_folder' => 'img/default/', // Default image folder
 
     /*
     |--------------------------------------------------------------------------
@@ -26,7 +27,7 @@ return [
      */
 
     'allowed_types_check' => false,
-    'allowed_types'       => [
+    'allowed_types' => [
         'application/msword',
         'application/pdf',
         'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
