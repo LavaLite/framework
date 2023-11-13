a06cba09da27087b2013035c38c536491e4f83ad
George John <george@renfos.com>
2023-11-13 15:31:32 +0400

    Update release packages

--- bin/release.sh
+++ bin/release.sh
@@ -52,14 +52,14 @@ git tag $VERSION
 git push origin --tags
 
 # Tag Components
-for REMOTE in auth broadcasting bus cache collections conditionable config console container contracts cookie database encryption events filesystem hashing http log macroable mail notifications pagination pipeline process queue redis routing session support testing translation validation view
+for REMOTE in Actions Database Filer Foremove Hashids Http Install Log Master Menu Node Notification Repository Role Setting Team Theme Trans User Validator Workflow
 do
     echo ""
     echo ""
     echo "Releasing $REMOTE";
 
-    TMP_DIR="/tmp/laravel-split"
-    REMOTE_URL="git@github.com:illuminate/$REMOTE.git"
+    TMP_DIR="/tmp/litepie-split"
+    REMOTE_URL="git@github.com:Litepie/$REMOTE.git"
 
     rm -rf $TMP_DIR;
     mkdir $TMP_DIR;
